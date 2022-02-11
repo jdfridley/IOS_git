7529869f93639b338ac328721e0c0c977db4e826
Jason Fridley <fridley@syr.edu>
2022-02-11 11:31:15 -0500

    finalized output data

--- licor_import_IOS_France3.R
+++ licor_import_IOS_France3.R
@@ -17,7 +17,8 @@ unique(master$Sampling.Date)
 unique(master$Site.final)
 
 #folder of licor files ***use the current France folder on as-fridley (correct files and filenames)
-folder.local = "/Users/fridley/Documents/academic/projects/IOS_FranceJapan/licor_files/LICOR data files_France/" #downloaded after Robert's fixes, 2-8-22
+#folder.local = "/Users/fridley/Documents/academic/projects/IOS_FranceJapan/licor_files/LICOR data files_France/" #downloaded after Robert's fixes, 2-8-22
+folder.local = "C:\\jdfhome\\NSFjapanfrance\\LICOR\\LICOR data files_France\\" #on PC
 files = list.files(folder.local)
 length(files)
 
@@ -40,12 +41,13 @@ setdiff(ena.labels,files)
 #2020: all text files have 36 columns, 2 xlsx files have 56 columns
 #2021: all text files have 36 columns
 
+i = 1 #needed for an unknown reason
+
 #output
 out = NULL
 out19 = NULL
 out19xls = NULL
 out20xls = NULL
-#test = rep(0,length(files))
 
 #loop over each file, examine, and add to 'out'
 for(i in i:length(files)) {
@@ -56,22 +58,19 @@ site = master$Site.final[master$LICOR.file.in.handENA==files[i]]
 species = master$Species[master$LICOR.file.in.handENA==files[i]]
 code = master$Species.abbreviation[master$LICOR.file.in.handENA==files[i]]
 
+print(i)
+
 #1. 2020 and 2021 text files: all have 36 columns
 if(gregexpr(".",files[i],fixed=T)<0 & substr(files[i],1,4)!="2019") { #tab delim text files from 2020, 2021
   dat = read.table(paste0(folder.local,files[i]),header=F,blank.lines.skip=F,sep="\t")
   start = which(substr(dat[,1],1,3)=="Obs") - 1
   dat = read.csv(paste0(folder.local,files[i]),header=T,skip=start,sep="\t")
-  #test[i] = dim(dat)[2]
   dat = dat[(!is.na(dat$FTime)),] #remove rows of no data
-  #if(dim(dat)[2]<80) {
-  #  dat[,59:83] = NA  #add extra rows (missing in this version of the licor files)
-  #}
   dat$filename = files[i] #append column of filename
   dat$date = date
   dat$site = site
   dat$species = species
   dat$sppcode = code
-  #names(dat) = names(out)
   par(mfrow=c(1,2))
   plot(dat$PARi,dat$Photo,pch=19,cex=1.3,main="A-q")
   plot(dat$Ci,dat$Photo,pch=19,cex=1.3,main="A-Ci")
@@ -80,23 +79,19 @@ if(gregexpr(".",files[i],fixed=T)<0 & substr(files[i],1,4)!="2019") { #tab delim
   #readline() #uncomment if you want to inspect each curve upon import
 }
 
+print(i)
 
 #2. 2019 text files: 58 columns
 if(gregexpr(".",files[i],fixed=T)<0 & substr(files[i],1,4)=="2019") { #tab delim text files from 2019
   dat = read.table(paste0(folder.local,files[i]),header=F,blank.lines.skip=F,sep="\t")
   start = which(substr(dat[,1],1,3)=="Obs") - 1
   dat = read.csv(paste0(folder.local,files[i]),header=T,skip=start,sep="\t")
-  #test[i] = dim(dat)[2]
   dat = dat[(!is.na(dat$FTime)),] #remove rows of no data
-  #if(dim(dat)[2]<80) {
-  #  dat[,59:83] = NA  #add extra rows (missing in this version of the licor files)
-  #}
   dat$filename = files[i] #append column of filename
   dat$date = date
   dat$site = site
   dat$species = species
   dat$sppcode = code
-  #names(dat) = names(out)
   par(mfrow=c(1,2))
   plot(dat$PARi,dat$Photo,pch=19,cex=1.3,main="A-q")
   plot(dat$Ci,dat$Photo,pch=19,cex=1.3,main="A-Ci")
@@ -105,6 +100,7 @@ if(gregexpr(".",files[i],fixed=T)<0 & substr(files[i],1,4)=="2019") { #tab delim
   #readline() #uncomment if you want to inspect each curve upon import
 }
 
+print(i)
 
 #3. 2019 excel files
 if(str_sub(files[i],start=-4) == "xlsx" & substr(files[i],1,4)=="2019") { #excel files of 2019
@@ -112,7 +108,6 @@ if(str_sub(files[i],start=-4) == "xlsx" & substr(files[i],1,4)=="2019") { #excel
   start = which(dat[,1]=="Obs") - 1
   dat = as.data.frame(read_excel(paste0(folder.local,files[i]),col_names=TRUE,skip=start))
   dat = dat[-1,]
-  #test[i] = dim(dat)[2]
   dat = dat[(!is.na(dat$FTime)),] #remove rows of no data
   dat[,c(1,3:83)] = apply(dat[,c(1,3:83)], 2, function(x) as.numeric(as.character(x))) #change character to numeric (all except HHMMSS)
   dat$filename = files[i] #append column of filename
@@ -128,6 +123,7 @@ if(str_sub(files[i],start=-4) == "xlsx" & substr(files[i],1,4)=="2019") { #excel
   #readline() #uncomment if you want to inspect each curve upon import
 }
 
+print(i)
 
 #4. 2020 excel files
 if(str_sub(files[i],start=-4) == "xlsx" & substr(files[i],1,4)=="2020") { #excel files of 2019
@@ -135,7 +131,6 @@ if(str_sub(files[i],start=-4) == "xlsx" & substr(files[i],1,4)=="2020") { #excel
   start = which(dat[,1]=="Obs") - 1
   dat = as.data.frame(read_excel(paste0(folder.local,files[i]),col_names=TRUE,skip=start))
   dat = dat[-1,]
-  #test[i] = dim(dat)[2]
   dat = dat[(!is.na(dat$FTime)),] #remove rows of no data
   dat[,c(1,3:56)] = apply(dat[,c(1,3:56)], 2, function(x) as.numeric(as.character(x))) #change character to numeric (all except HHMMSS)
   dat$filename = files[i] #append column of filename
@@ -151,10 +146,21 @@ if(str_sub(files[i],start=-4) == "xlsx" & substr(files[i],1,4)=="2020") { #excel
   #readline() #uncomment if you want to inspect each curve upon import
 }
 
+print(i)
+
 } #close files loop
 
+dim(out)
+dim(out19)
+dim(out19xls)
+dim(out20xls)
+
 #combine 4 out files, getting the columns in the right places
-test = bind_rows(out,out19) #from dplyr
-#***note: doing its thing again, not saving data: put in speed breaks or use readline?
+out1 = bind_rows(out,out19) #from dplyr
+out1[,c(1,3:36,42:63)] = apply(out1[,c(1,3:36,42:63)], 2, function(x) as.numeric(as.character(x))) #change character to numeric (all except HHMMSS)
+out2 = bind_rows(out19xls,out20xls)
+out.final = bind_rows(out1,out2)
+str(out.final)
+  #164 files
 
 #write.csv(out,file="/Users/fridley/Documents/academic/projects/IOS_FranceJapan/licor_files/France_licor2.csv")
